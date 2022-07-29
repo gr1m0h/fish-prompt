814ceff04551172b6934896d927d64264b845947
grimoh <dev@grimoh.net>
2022-07-30 01:17:17 +0900

    feat: Update color

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,13 +2,13 @@ function fish_prompt
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color cyan white
+                set_color bd93f9 purple
             case insert
-                set_color green white
+                set_color 50fa7b green
             case replace-one
-                set_color green white
+                set_color ffb86c magenta
             case visual
-                set_color yellow white
+                set_color f1fa8c yellow
             end
         echo (prompt_pwd) "> "
     end
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,8 +1,10 @@
+# output fit_branch
 function git_branch
     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 end
 
+# right side prompt
 function fish_right_prompt
-    set_color purple white
+    set_color ff79c6 brred
     echo (git_branch)
 end
