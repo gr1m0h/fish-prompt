53bbdbc33e8eb48e19d9a9730c15b050bed5b934
grimoh <dev@grimoh.net>
2022-07-30 01:13:33 +0900

    style: eclint fix

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,16 +1,15 @@
 function fish_prompt
-	if test "$fish_key_bindings" = "fish_vi_key_bindings"
-		switch $fish_bind_mode
-			case default
-				set_color cyan white
-			case insert
-				set_color green white
-			case replace-one
-				set_color green white
-			case visual
-				set_color yellow white
-			end
-		echo (prompt_pwd) "> "
-	end
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color cyan white
+            case insert
+                set_color green white
+            case replace-one
+                set_color green white
+            case visual
+                set_color yellow white
+            end
+        echo (prompt_pwd) "> "
+    end
 end
-
--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,8 +1,8 @@
 function git_branch
-	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 end
 
 function fish_right_prompt
-	set_color purple white
-	echo (git_branch)
+    set_color purple white
+    echo (git_branch)
 end
