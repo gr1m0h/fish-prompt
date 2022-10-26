e9e5a134271fe0af6d68ed2480dd24b667f193f0
gr1m0h <dev@grimoh.net>
2022-10-26 16:19:54 +0900

    Update and rename fish_prompt.fish to __fish_prompt.fish

--- functions/__fish_prompt.fish
+++ functions/__fish_prompt.fish
@@ -1,3 +1,5 @@
+function fish_mode_prompt; end
+
 function fish_prompt
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
@@ -12,4 +14,15 @@ function fish_prompt
             end
         echo (prompt_pwd) "> "
     end
+end
+
+# output git_branch
+function git_branch
+    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+end
+
+# right side prompt
+function fish_right_prompt
+    set_color ff79c6 brred
+    echo (git_branch)
 end
