6d86915f7657761bb49716cd0db695af3d7ac3c7
grimoh <dev@grimoh.net>
2023-02-26 00:36:18 +0900

    Update prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -3,29 +3,25 @@ function fish_mode_prompt; end
 # vi mode
 fish_vi_key_bindings
 
+# prompt
 function fish_prompt
     if test "$fish_key_bindings" = "fish_vi_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color bd93f9 purple
+                set_color bd93f9
             case insert
-                set_color 50fa7b green
+                set_color 50fa7b
             case replace-one
-                set_color ffb86c magenta
+                set_color ff5555
             case visual
-                set_color f1fa8c yellow
+                set_color ff79c6
             end
-        echo (prompt_pwd) "> "
+        echo (prompt_pwd)(__fish_git_prompt) "> "
     end
-end
-
-# output git_branch
-function git_branch
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 end
 
 # right side prompt
 function fish_right_prompt
-    set_color ff79c6 brred
-    echo (git_branch)
+    set_color f1fa8c
+    echo $CMD_DURATION
 end
